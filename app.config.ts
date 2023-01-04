39380b1a939899635020184c3659eae533f31c4f
markthree <1801982702@qq.com>
2023-01-04 19:38:32 +0800

    chore: clearer icons config

--- app.config.ts
+++ app.config.ts
@@ -3,24 +3,29 @@ export default defineAppConfig({
 		title: 'Quick',
 		description:
 			'一个编程快速入门视频栏目，帮助你快速学会编程知识',
+		url: 'https://dishait.github.io/quick/',
+
 		socials: {
+			bilibili: {
+				icon: 'fa-brands:youtube',
+				label: 'bilibili',
+				href: 'https://space.bilibili.com/590784571'
+			},
 			github: 'dishait/quick'
 		},
 		header: {
 			title: 'Quick',
 			showLinkIcon: true
 		},
+		github: {
+			edit: true,
+			contributors: true
+		},
 		footer: {
 			credits: {
 				text: 'dishait.cn',
 				href: 'http://dishaxy.dishait.cn/'
-			},
-			iconLinks: [
-				{
-					icon: 'fa-brands:youtube',
-					href: 'https://space.bilibili.com/590784571'
-				}
-			]
+			}
 		}
 	}
 })
