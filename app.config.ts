c0c816ad601b814b4be2a7dd18e4cc1e5390c8de
markthree <1801982702@qq.com>
2023-01-04 16:35:11 +0800

    refactor(docs): more beautiful and clear home page

--- app.config.ts
+++ app.config.ts
@@ -3,26 +3,22 @@ export default defineAppConfig({
 		title: 'Quick',
 		description:
 			'The best place to start your documentation.',
-		image:
-			'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
 		socials: {
-			twitter: 'nuxt_themes',
-			github: 'nuxt-themes/docus'
-		},
-		aside: {
-			level: 0,
-			exclude: []
+			github: 'dishait/quick'
 		},
 		header: {
-			logo: true,
-			showLinkIcon: true,
-			exclude: []
+			title: 'Quick',
+			showLinkIcon: true
 		},
 		footer: {
+			credits: {
+				text: 'dishait.cn',
+				href: 'http://dishaxy.dishait.cn/'
+			},
 			iconLinks: [
 				{
-					href: 'https://nuxt.com',
-					icon: 'IconNuxtLabs'
+					icon: 'fa-brands:youtube',
+					href: 'https://space.bilibili.com/590784571'
 				}
 			]
 		}
