72020440d0a541c896ca3f82ab3ab4233abcf8e7
markthree <1801982702@qq.com>
2023-01-06 18:22:30 +0800

    feat(rollback): remove pageTransition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,8 @@
-import { createResolver } from '@nuxt/kit'
-
-const { resolve: resolveThemeDir } = createResolver(
-	import.meta.url
-)
-
 export default defineNuxtConfig({
 	extends: '@nuxt-themes/docus',
 	app: {
 		pageTransition: { name: 'page', mode: 'out-in' }
 	},
-	css: [resolveThemeDir('./assets/global.css')],
 	experimental: {
 		asyncEntry: true // 异步入口
 	},
