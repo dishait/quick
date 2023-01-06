fa2149a58526014e8a42cd31efe47651bb884480
markthree <1801982702@qq.com>
2023-01-06 20:32:35 +0800

    feat: add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
 		preset: 'deno',
 		prerender: {
 			crawlLinks: true,
-			routes: ['/', '/start', '/about']
+			routes: ['/', '/start', '/about', '/sitemap.xml']
 		}
 	}
 })
