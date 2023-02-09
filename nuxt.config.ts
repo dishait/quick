738f3b48f05673778a7d3c8c99830331f8e9cdfa
markthree <1801982702@qq.com>
2023-02-09 22:01:20 +0800

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,51 +1,50 @@
 import {
-	genPrerenderRoutesSync,
-	createDefaultGenPrerenderRoutesSyncOptions
-} from 'nuxt3-intelligence'
+  createDefaultGenPrerenderRoutesSyncOptions,
+  genPrerenderRoutesSync,
+} from "nuxt3-intelligence";
 
-const defaultOptions =
-	createDefaultGenPrerenderRoutesSyncOptions()
-const routes = genPrerenderRoutesSync(defaultOptions)
+const defaultOptions = createDefaultGenPrerenderRoutesSyncOptions();
+const routes = genPrerenderRoutesSync(defaultOptions);
 
-routes.push('/sitemap.xml', '/config')
+routes.push("/sitemap.xml", "/config");
 
 export default defineNuxtConfig({
-	extends: '@nuxt-themes/docus',
-	modules: ['@unocss/nuxt'],
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: 'zh-CN'
-			}
-		}
-	},
-	unocss: {
-		uno: true,
-		attributify: true
-	},
-	experimental: {
-		asyncEntry: true, // 异步入口
-		reactivityTransform: true // 开启响应性语法糖
-	},
-	vite: {
-		define: {
-			__VUE_OPTIONS_API__: false // 明确不使用 options api
-		}
-	},
-	sourcemap: false,
-	typescript: {
-		shim: false
-	},
-	nitro: {
-		minify: true,
-		preset: 'deno',
-		compressPublicAssets: {
-			gzip: true,
-			brotli: true
-		},
-		prerender: {
-			routes,
-			crawlLinks: true
-		}
-	}
-})
+  extends: "@nuxt-themes/docus",
+  modules: ["@unocss/nuxt"],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "zh-CN",
+      },
+    },
+  },
+  unocss: {
+    uno: true,
+    attributify: true,
+  },
+  experimental: {
+    asyncEntry: true, // 异步入口
+    reactivityTransform: true, // 开启响应性语法糖
+  },
+  vite: {
+    define: {
+      __VUE_OPTIONS_API__: false, // 明确不使用 options api
+    },
+  },
+  // sourcemap: false,
+  typescript: {
+    shim: false,
+  },
+  nitro: {
+    minify: true,
+    preset: "deno",
+    compressPublicAssets: {
+      gzip: true,
+      brotli: true,
+    },
+    prerender: {
+      routes,
+      crawlLinks: true,
+    },
+  },
+});
