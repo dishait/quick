17056dfc97d608b49af82545dbe547a4a8815830
markthree <1801982702@qq.com>
2023-02-17 15:53:33 +0800

    chore(content): add course

--- components/content/card-link.vue
+++ components/content/card-link.vue
@@ -6,25 +6,14 @@ defineProps<{
 </script>
 
 <template>
-	<div
-		class="mt-5 font-semibold text-md text-gray-400 flex justify-end space-x-6">
-		<NuxtLink
-			v-if="doc"
-			:to="doc"
-			class="flex items-center space-x-1 cursor-pointer hover:text-emerald-700">
-			<Icon
-				name="teenyicons:documents-outline"
-				size="16px"></Icon>
+	<div class="mt-5 font-semibold text-md text-gray-400 flex justify-end space-x-6">
+		<NuxtLink v-if="doc" :to="doc" class="flex items-center space-x-1 cursor-pointer hover:text-cyan-600">
+			<Icon name="teenyicons:documents-outline" size="16px"></Icon>
 			<span> 文档 </span>
 		</NuxtLink>
-		<NuxtLink
-			v-if="video"
-			:to="video"
-			target="__blank"
-			class="flex items-center space-x-1 cursor-pointer hover:text-emerald-700">
-			<Icon
-				name="ic:outline-video-library"
-				size="20px"></Icon>
+		<NuxtLink v-if="video" :to="video" target="__blank"
+			class="flex items-center space-x-1 cursor-pointer hover:text-cyan-600">
+			<Icon name="ic:outline-video-library" size="20px"></Icon>
 
 			<span> 视频 </span>
 		</NuxtLink>
