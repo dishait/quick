a2ba8fb32f770c88efd7c49a2c826d0f05bed2b0
markthree <1801982702@qq.com>
2023-02-26 18:20:27 +0800

    feat: add video preview

---
+++ components/content/iframe-video.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { page = 1 } = defineProps<{
+    aid: string,
+    cid: string,
+    bvid: string,
+    page?: number,
+}>()
+</script>
+
+<template>
+    <iframe
+        :src="`//player.bilibili.com/player.html?aid=${aid}&bvid=${bvid}&cid=${cid}&page=${page}&high_quality=1&danmaku=1`"
+        allowfullscreen="true" width="100%" height="450" scrolling="no" frameborder="0"
+        sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts allow-popups"></iframe>
+</template>
