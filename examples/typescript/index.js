8ad030b83a3ad72de74029823fe83b5867633963
markthree <1801982702@qq.com>
2023-03-25 17:50:32 +0800

    docs(examples/typescript): init

---
+++ examples/typescript/index.js
@@ -0,0 +1,6 @@
+// index.ts
+function greeter(name) {
+    return "Hello, ".concat(name, "!");
+}
+var greeting = greeter('TypeScript');
+console.log(greeting);
---
+++ examples/typescript/index.ts
@@ -0,0 +1,7 @@
+// index.ts
+function greeter(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+const greeting = greeter('TypeScript');
+console.log(greeting);
